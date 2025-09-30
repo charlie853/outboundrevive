cad612f98d98818b883b670bff23cdbf3a0a45fe
Charlie <charlie@outboundrevive.com>
2025-09-30 18:27:33 -0400

    Add server action stub: app/actions/pick.ts

---
+++ app/actions/pick.ts
@@ -0,0 +1,16 @@
+// app/actions/pick.ts
+'use server';
+
+/**
+ * Minimal server action stub.
+ * Works with BOTH:
+ *   import { pick } from '../actions/pick'
+ * and
+ *   import pick from '../actions/pick'
+ */
+export async function pick(..._args: any[]) {
+  // TODO: replace with real logic
+  return { ok: true };
+}
+
+export default pick;
